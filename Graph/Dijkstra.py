59ac88d576097b55ca6a865b4210eebf8a55faae
Yuki Shimoyamada <azsedcftgbhujmkol0427@gmail.com>
2021-07-27 03:05:05 +0900

    improve dijkstra

--- Graph/Dijkstra.py
+++ Graph/Dijkstra.py
@@ -1,38 +1,43 @@
-from copy import copy
-# V: 頂点数
-# g[v] = {(w, cost)}:
-#     頂点vから遷移可能な頂点(w)とそのコスト(cost)
-# r: 始点の頂点
-
 from heapq import heappush, heappop
-INF = 10**10
 
 
-def dijkstra(N, G, s):
-    global dist
-    dist = [INF] * N
-    que = [(0, s)]
-    dist[s] = 0
-    while que:
-        c, v = heappop(que)
-        if dist[v] < c:
+def dijkstra(n, graph, start):
+    """
+    graph[u] := [(cost, v), ...]
+    """
+    distance = [float('inf') for _ in range(n)]
+    distance[start] = 0
+    Q = [(0, start)]
+    while Q:
+        u_cost, u = heappop(Q)
+        if u_cost > distance[u]:
             continue
-        for t, cost in G[v]:
-            if dist[v] + cost < dist[t]:
-                dist[t] = dist[v] + cost
-                heappush(que, (dist[t], t))
+        for cost, v in graph[u]:
+            if distance[v] > distance[u] + cost:
+                distance[v] = distance[u] + cost
+                heappush(Q, (distance[v], v))
+    return distance
 
 
-N, M = map(int, input().split())
-G = [[] for i in range(N)]
-for i in range(M):
-    a, b, c = map(int, input().split())
-    G[a - 1].append((b - 1, c))
-    G[b - 1].append((a - 1, c))
-GG = copy(G)
-dijkstra(N, G, 0)
-A = dist
-dijkstra(N, GG, N - 1)
-B = dist
-for i, j in zip(A, B):
-    print(i + j)
+def dijkstra_test(n, graph, start):
+    """
+    graph[u] := [(cost, v), ...]
+        u -- cost --> v
+    """
+    # 初期化
+    distance = [float('inf') for _ in range(n)]
+    distance[start] = 0
+    # 始点を queue に追加
+    Q = [(0, start)]
+    while Q:
+        u_cost, u = heappop(Q)
+        # u_cost が u までの距離より大きかったら破棄
+        if u_cost > distance[u]:
+            continue
+        # u に更新があれば行けるところも更新
+        for cost, v in graph[u]:
+            # v までの距離が u + cost より大きかったら更新して queue に追加
+            if distance[v] > distance[u] + cost:
+                distance[v] = distance[u] + cost
+                heappush(Q, (distance[v], v))
+    return distance
