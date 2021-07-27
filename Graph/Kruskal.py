c4ca579223e4d03873d8d34d4cbda35ad5ea9bb1
Yuki Shimoyamada <azsedcftgbhujmkol0427@gmail.com>
2021-07-27 17:56:39 +0900

    improve kruskal

--- Graph/Kruskal.py
+++ Graph/Kruskal.py
@@ -1,58 +1,68 @@
-
-import collections
-import array
-
-Edge = collections.namedtuple("Edge", "start end weight")
+from collections import defaultdict
 
 
-class UnionFind(object):
+class UnionFind():
+    def __init__(self, n):
+        self.n = n
+        self.parents = [-1] * n
 
-    def __init__(self, number_of_nodes):  # 初期化
-        self.par = array.array("L", range(number_of_nodes))
-        self.rank = array.array("L", (0 for i in range(number_of_nodes)))
-
-    def root(self, node):  # 根を求める
-        if self.par[node] == node:
-            return node
+    def find(self, x):
+        if self.parents[x] < 0:
+            return x
         else:
-            r = self.root(self.par[node])
-            self.par[node] = r  # 経路圧縮
-            return r
+            self.parents[x] = self.find(self.parents[x])
+            return self.parents[x]
 
-    def in_the_same_set(self, node1, node2):  # 同じ集合に属するか
-        return self.root(node1) == self.root(node2)
+    def union(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
 
-    def unite(self, node1, node2):  # 属する集合を併合
-        x = self.root(node1)
-        y = self.root(node2)
         if x == y:
-            pass
-        elif self.rank[x] < self.rank[y]:
-            self.par[x] = y
-        else:
-            self.par[y] = x
-            if self.rank[x] == self.rank[y]:
-                self.rank[x] += 1
+            return
+
+        if self.parents[x] > self.parents[y]:
+            x, y = y, x
+
+        self.parents[x] += self.parents[y]
+        self.parents[y] = x
+
+    def size(self, x):
+        return -self.parents[self.find(x)]
+
+    def same(self, x, y):
+        return self.find(x) == self.find(y)
+
+    def members(self, x):
+        root = self.find(x)
+        return [i for i in range(self.n) if self.find(i) == root]
+
+    def roots(self):
+        return [i for i, x in enumerate(self.parents) if x < 0]
+
+    def group_count(self):
+        return len(self.roots())
+
+    def all_group_members(self):
+        group_members = defaultdict(list)
+        for member in range(self.n):
+            group_members[self.find(member)].append(member)
+        return group_members
+
+    def __str__(self):
+        return '\n'.join(f'{r}: {m}' for r, m in self.all_group_members().items())
 
 
-# kruskal法により最小全域木を求める
-def compute_mst_kruskal(max_v, edges):
-    edges.sort(key=lambda edge: edge.weight)
-    uf = UnionFind(max_v)
-    mst = []
+def kruskal(V, edges):
+    """
+    edges := [(start, end, weight)]
+    """
+    # weight で sort
+    edges = sorted(edges, key=lambda x: x[2])
+    uf = UnionFind(V)
+    minimum_spanning_tree = []
     for edge in edges:
-        if not uf.in_the_same_set(edge.start, edge.end):
-            uf.unite(edge.start, edge.end)
-            mst.append(edge)
-    return mst
-
-
-def main():
-    max_v, max_e, r = map(int, input().split())
-    edges = [Edge(*map(int, input().split())) for _ in range(max_e)]
-    mst = compute_mst_kruskal(max_v, edges)
-    print(sum(edge.weight for edge in mst))
-
-
-if __name__ == '__main__':
-    main()
+        start, end, weight = edge
+        if not uf.same(start, end):
+            uf.union(start, end)
+            minimum_spanning_tree.append(edge)
+    return minimum_spanning_tree
